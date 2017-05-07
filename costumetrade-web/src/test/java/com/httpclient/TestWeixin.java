6d3235abbf0682eb50011c166d76577d84003261
chenyoujing <chenyoujing2008@sina.com>
2017-05-07 18:30:57 +0800

    配置

--- costumetrade-web/src/test/java/com/httpclient/TestWeixin.java
+++ costumetrade-web/src/test/java/com/httpclient/TestWeixin.java
@@ -1,18 +1,18 @@
 package com.httpclient;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import me.chanjar.weixin.cp.api.WxCpService;
 import me.chanjar.weixin.cp.bean.WxCpMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class TestWeixin {
 	@Autowired
-	private static  WxCpService wxService;
+	private WxCpService wxService;
 	public static void main(String[] args) {
 		WxCpMessage.WxArticle article = new WxCpMessage.WxArticle();
 		String userIds ="";
 		//wxService.messageSend(WxCpMessage.NEWS().agentId("1").toUser(userIds).addArticle(article).build());
-
+		
 	}
 
 }
